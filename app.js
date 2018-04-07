b7bf5ccb819b8e1299cd30f0e2a71e5860c16de0
Ted Stark <info4tws@gmail.com>
2018-04-07 00:32:19 -0700

    Renames and cleanup of folders and files.

--- app.js
+++ app.js
@@ -1,46 +1,57 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const createError = require('http-errors');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+require('dotenv').config();
 
 //For Timestamp messages in console
 require('console-stamp')(console, 'HH:MM:ss');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var twilioRouter = require('./routes/twilio');
+let app = express();
+let env = process.env;
+let indexRouter = require('./routes/routes_app');
+let usersRouter = require('./routes/routes_users');
+let twilioRouter = require('./routes/routes_twilio');
 
-var app = express();
-var env = process.env;
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-// route use statements
+// Route statements
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/twilio', twilioRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+// Load View Engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+//Set Public folder path
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Middleware
+    app.use(logger('dev'));
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: false }));
+    app.use(cookieParser());
+
+    //Body Parser Middleware
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: false }));
+
+// Error Handler
+    // catch 404 and forward to error handler
+    app.use(function(req, res, next) {
+      next(createError(404));
+    });
+
+    // error handler
+    app.use(function(err, req, res, next) {
+      // set locals, only providing error in development
+      res.locals.message = err.message;
+      res.locals.error = req.app.get('env') === 'development' ? err : {};
+      // render the error page
+      res.status(err.status || 500);
+      res.render('page_error');
+    });
 
 module.exports = app;
--- routes/routes_twilio.js
+++ routes/routes_twilio.js
@@ -1,33 +1,36 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const bodyParser = require('body-parser');
 
-// Secure sensitive info in environment file
-// require('dotenv').config();
-// var env = process.env;
-
-// Twilio Credentials
-const accountSid = '';
-const authToken = '';
-// const accountSid = process.env.TWILIO_SID;
-// const authToken = process.env.TWILIO_AUTH;
+// Secure Twilio Credentials
+require('dotenv').load();
+let env = process.env;
 
 // require the Twilio module and create a REST client
-const client = require('twilio')(accountSid, authToken);
+const client = require('twilio')(env.TWILIO_SID, env.TWILIO_AUTH);
 
-/* GET twilio message send. */
-router.post('/', function(req, res) {
-        client.messages
-            .create({
-                to: '+16232524833',
-                from: '+16027867178',
-                body: 'Hello! This is from our FSWF application!',
-            })
-            .then(message => console.log(message.sid));
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({ extended: false }));
+
+// GET: Show 'Messages' Page
+router.get('/messages', function(req,res){
+    res.render('page_messages', {
+        title: 'Send a Message'
     });
+});
 
-router.get('/records', function (req,res) {
-    client.usage.records.today
-        .each({category: 'sms'}, (record) => console.log(record.usage));
+
+// POST: Send a Message
+router.post('/messages', function(req, res) {
+    client.messages
+        .create({
+            to: '+1'+req.body.msgToNum,
+            from: '+16027867178',
+            body: req.body.msgCustom+'  From: '+req.body.msgFrom+' Please call '+req.body.msgCBkNum+' with any changes.',
+        })
+        .then(message => console.log(message.sid));
+    console.log('Submitted!')
+    res.redirect('/twilio/messages');
 });
 
 module.exports = router;
