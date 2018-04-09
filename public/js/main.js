5e2b64271a590f3dea58de8fcea9322da419c77f
Ted Stark <info4tws@gmail.com>
2018-04-09 09:15:18 -0700

    Multi-receipient functionality in place

--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,24 @@
-//
-//
-// $(document).ready(function () {
-//     init();
-// });
-//
-// function init(){
-//     enable();
-// }
-//
-// function enable (){
-// }
-//
+$(document).ready(function () {
+    init();
+});
 
+function init(){
+    enable();
+}
+
+function enable () {
+  $('.addNum').on('click', addNumFields);
+  $('.removeNum').on('click', remNumFields);
+  // chgNumFields();
+}
+
+function addNumFields () {
+  $('.multiFieldWrapper').each(function() {
+      $('.multiField:first-child .removeNum', $('.multiFields', this)).attr('hidden',false)
+      $('.multiField:first-child', $('.multiFields', this)).clone(true).appendTo($('.multiFields', this)).find('input').val('').focus();
+      $('.multiField:first-child .removeNum', $('.multiFields', this)).attr('hidden',true)
+  });
+}
+function remNumFields () {
+  $(this).parent('.multiField').remove();
+}
--- routes/routes_twilio.js
+++ routes/routes_twilio.js
@@ -7,7 +7,7 @@ require('dotenv').load();
 let env = process.env;
 
 // require the Twilio module and create a REST client
-const client = require('twilio')(env.TWILIO_SID, env.TWILIO_AUTH);
+const twilio = require('twilio')(env.TWILIO_SID, env.TWILIO_AUTH);
 
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended: false }));
@@ -22,14 +22,38 @@ router.get('/messages', function(req,res){
 
 // POST: Send a Message
 router.post('/messages', function(req, res) {
-    client.messages
+    var values = $('.msgToNum').map(function() {return this.value; }).get();
+    twilio.messages
         .create({
             to: '+1'+req.body.msgToNum,
             from: '+16027867178',
             body: req.body.msgCustom+'  From: '+req.body.msgFrom+' Please call '+req.body.msgCBkNum+' with any changes.',
         })
         .then(message => console.log(message.sid));
-    console.log('Submitted!')
+    console.log('Submitted!');
+    console.log(values);
+    res.redirect('/twilio/messages');
+});
+
+// POST: Send several Messages
+router.post('/moremessages', function(req, res) {
+    // var values = $('.msgToNum').map(function() {return this.value; }).get();
+    const numbers = ['+16236403640', '+14802720635', '+14804131123', '+16232524833']
+    for (i in numbers) {
+      console.log(numbers[i]);
+      twilio.messages
+        .create(
+          {
+            to: numbers[i],
+            from: 'MG1eadb76e27a933ad7fff82a0a3af7313',
+            body: '**DO NOT REPLY** Hey Team. I figured out how to send to multiple numbers at once through Twilio. This was sent to all of you via one POST route.'
+          }
+      )
+        .then(message => {
+          console.log(message.sid);
+          console.log('Submitted!');
+        });
+      }
     res.redirect('/twilio/messages');
 });
 
