737e1dab0267ec96389a0bc899adf8bdb444414b
AZJoan <joanpadams@gmail.com>
2018-04-10 15:19:14 -0700

    fixed date time errors added warning window

--- public/app.js
+++ public/app.js
@@ -5,22 +5,13 @@ const app = express();
 const path = require('path');
 const dateformat = require('dateformat');
 const moment = require('moment-timezone');
+const dialog = require('dialog');
 
-//const popupS = require('popups');
 
-// Global Vars
+// Global Variables
 app.use(function(req, res, next){
    res.locals.errors = "";
    res.locals.textMsg = "";
-   res.locals.errors = "";
-   res.locals.customMsg = "";
-   res.locals.customFrom = "";
-   res.locals.fromGroup = "";
-   res.locals.callback = "";
-   res.locals.client = "";
-   res.locals.appDate ="";
-   res.locals.appTime = "";
-
    next();
 });
 
@@ -28,6 +19,10 @@ app.use(express.static('public/images'));
 
 app.get('/css/style.css', function(req, res){
     res.sendFile(__dirname + '/css/style.css');
+});
+
+app.get('/views/preview.pug', function(req, res){
+    res.sendFile(__dirname + '/views/preview.pug');
 });
 
 // View Engine
@@ -60,64 +55,117 @@ app.use(expresValidator({
 }));
 
 app.get('/', function(req, res){
-   res.render('message');
+   console.log("get / index");
+   res.render('index');
 });
 
-app.post('/msg/add', function(req, res){
+app.get('/reminder', function(req, res){
+    console.log("get /reminder");
+    res.render('reminder');
+});
+
+app.get('/message', function(req, res){
+    console.log("get /message");
+    res.render('message');
+});
+
+app.post('/submitcus', function(req, res){
+    console.log("post /submitcus");
 
     req.checkBody('txtCustomMsg', 'Custom message is required').notEmpty();
-    req.checkBody('txtCallback', 'Custom message is required').notEmpty();
+    req.checkBody('txtCallback', 'Callback number is required').notEmpty();
     req.checkBody('txtClient', 'Client number is required').notEmpty();
 
-    errors = req.validationErrors();
-    var err1 = errors.txtCustomMsg;
-    var err2 = errors.txtCallback;
-    var err3 = errors.txtClient;
+    var cerrors = req.validationErrors();
 
-    if (errors) {
+    if (cerrors) {
         errmsgs = "Error: Must provide all required fields";
         console.log(errmsgs);
-
-       // console.log("errors.txtCustomMsg:" + err1);
-      //  console.log("errors.txtCallback:" + err2);
-       // console.log("errors.txtClient:" + err3);
-
-
-        //for (let i = 0; i < 3; i++) {
-        //     errmsgs = errmsgs + " " + errors(i);
-        //}
-        //res.render('message',{
-        //  err: errors,
-        //});
+        // display error warning popup window
+        dialog.info('Please provide all required fields.');
 
     } else {
 
-        customMsg = req.body.txtCustomMsg;
-        customFrom = req.body.txtCustomFrom;
-        fromGroup = req.body.txtFromGroup;
-        callback = req.body.txtCallback;
-        client = req.body.txtClient;
+        var ccustomMsg = req.body.txtCustomMsg;
+        var ccustomFrom = req.body.txtCustomFrom;
+        var cfromGroup = req.body.txtFromGroup;
+        var ccallback = req.body.txtCallback;
+        var cclient = req.body.txtClient;
 
-        // Format date from yyyy-mm-dd to m-d-yy
-        var tempDate = req.body.txtAppDate + " MST";
-        console.log("yyyy-mm-dd txtAppDate + MST = ", tempDate);
-        appDate = dateformat(tempDate, 'shortDate');
-        console.log("m-d-yyyy appDate=", appDate);
-
-        // Format time from 24 military, i.e. 17:30 to 9:30pm
-        console.log("txtAppTime= ", req.body.txtAppTime);
-        var tempDteTime = req.body.txtAppDate + "T" + req.body.txtAppTime + ":00";
-        console.log("txtAppDate + T + txtAppTime + :00 =", tempDteTime);
-        appTime = moment.tz(tempDteTime, "America/Phoenix").format('h:ma');
-        console.log("moment appTime= ", appTime);
-
-        textMsg = customMsg + " " + "Reminder for your appointment on " + appDate + " at " + appTime + "."
-            " If you have any questions please call us at " +
-            callback + ". Message sent by " + customFrom + " " + fromGroup;
+        textMsg = ccustomMsg + " If you have any questions please call us at " + ccallback + ". Message sent by " +
+        ccustomFrom + " " + cfromGroup;
 
         console.log(textMsg);
-    }
+
+        res.render('preview', { title:textMsg });
+
+    }  // if errors
+
+}); // app.post
+
+app.post('/submitrem', function(req, res){
+    console.log("post submitrem");
+
+    req.checkBody('txtRStandardMsg', 'Standard message is required').notEmpty();
+    req.checkBody('txtRCallback', 'Callback number is required').notEmpty();
+    req.checkBody('txtRClient', 'Client number is required').notEmpty();
+
+    var rerrors = req.validationErrors();
+
+    if (rerrors) {
+        errmsgs = "Error: Must provide all required fields";
+        console.log(errmsgs);
+        // display error warning popup window
+        dialog.info('Please provide all required fields.');
+
+    } else {
+
+        var rcustomMsg = req.body.txtRCustomMsg;
+        var rstandardMsg = req.body.txtRStandardMsg;
+        var rcustomFrom = req.body.txtRCustomFrom;
+        var rfromGroup = req.body.txtRFromGroup;
+        var rcallback = req.body.txtRCallback;
+        var rclient = req.body.txtRClient;
+
+
+        // Format date from yyyy-mm-dd to m-d-yy
+        var tempDate = req.body.txtRAppDate + " MST";
+        console.log("yyyy-mm-dd txtRAppDate + MST = ", tempDate);
+        var rappDate = dateformat(tempDate, 'shortDate');
+        console.log("m-d-yyyy appDate=", rappDate);
+
+        // Format time from 24 military, i.e. 17:30 to 9:30pm
+        console.log("txtRAppTime= ", req.body.txtRAppTime);
+        var tempDteTime = ('2018-01-01' + "T" + req.body.txtRAppTime);
+        console.log("2018-01-01 + T + txtRAppTime =", tempDteTime);
+        var rappTime = moment.tz(tempDteTime, "America/Phoenix").format('h:ma');
+        console.log("moment rappTime= ", rappTime);
+
+        textMsg = rcustomMsg + " " + rstandardMsg + rappDate + " at " + rappTime + "." +
+        " If you have any questions please call us at " + rcallback + ". Message sent by " +
+            rcustomFrom + " " + rfromGroup;
+
+        console.log(textMsg);
+
+        res.render('preview', { title: textMsg });
+
+    } // if errors
+}); // app.post submitrem
+
+
+app.post('/send', function(req, res, next) {
+    console.log("post /send");
+    console.log("textMsg = " + textMsg);
+    res.render('index');
+
 });
+
+app.get('/home', function(req, res, next) {
+    console.log("get /home");
+    res.render('index');
+
+});
+
 
 app.listen(3000, function() {
     console.log('app.js-server started on port 3000');
